af0b3f8342ee7a45bcceb4229f0c06e1ae89f7c7
Adam Gardner <adam@agardner.net>
2022-05-13 13:46:10 +1000

    Update install_script.sh

--- hello_world_tutorial/scripts/install_script.sh
+++ hello_world_tutorial/scripts/install_script.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+KEPTN_VERSION=0.14.2
+
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 && \
     chmod 700 get_helm.sh && \
     ./get_helm.sh
+
+wget https://github.com/keptn/keptn/releases/download/$KEPTN_VERSION/keptn-$KEPTN_VERSION-linux-amd64.tar.gz && \
+    tar -xf keptn-$KEPTN_VERSION-linux-amd64.tar.gz && \
+	cp keptn-$KEPTN_VERSION-linux-amd64 /usr/local/bin/keptn
