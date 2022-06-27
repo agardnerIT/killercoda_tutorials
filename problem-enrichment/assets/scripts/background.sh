77114ce0f3c0443ae28d941a283d88d3fe49a0db
adamgardnerit <adam@agardner.net>
2022-06-28 09:00:40 +1000

    update
    Signed-off-by: adamgardnerit <adam@agardner.net>

--- problem-enrichment/assets/scripts/background.sh
+++ problem-enrichment/assets/scripts/background.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 
-################################
-#     Installing Software      #
-#        Please wait           #
-################################
-
-######################
-# Installing stress  #
-######################
 apt install -y stress
 
-#########################
-# Adding ansible PPA    #
-#########################
 apt install -y software-properties-common
 apt-add-repository -y --update ppa:ansible/ansible
 
-#######################
-# Installing Ansible  #
-#######################
 apt install -y ansible
 
+# touch a file that foreground.sh is waiting for before proceeding
 touch /tmp/finished
-
-################################
-#   Installation Complete      #
-#        Please continue       #
-################################
