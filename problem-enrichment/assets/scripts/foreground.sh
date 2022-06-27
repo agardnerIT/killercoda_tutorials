5b2fad3d13d57f0d510c1af445935a4f90bd75e0
adamgardnerit <adam@agardner.net>
2022-06-27 17:51:04 +1000

    add foreground wait for background...
    Signed-off-by: adamgardnerit <adam@agardner.net>

---
+++ problem-enrichment/assets/scripts/foreground.sh
@@ -0,0 +1,8 @@
+echo "####################################################"
+echo "# Please wait while everything is installed...     #"
+echo "####################################################"
+while [ ! -f /tmp/finished ]; do sleep 1; done
+echo ""
+echo "####################################################"
+echo "# 🎉 Installation Complete. Please Continue...    #"
+echo "####################################################"
--- problem-enrichment/assets/scripts/install_script.sh
+++ problem-enrichment/assets/scripts/install_script.sh
@@ -2,3 +2,5 @@ apt update
 apt install software-properties-common
 apt-add-repository --yes --update ppa:ansible/ansible
 apt install ansible stress -y
+
+touch /tmp/finished
