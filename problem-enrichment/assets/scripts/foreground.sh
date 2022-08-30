2857b86ce8207cd2fee35784d164af071702bcf0
agardnerit <adam@agardner.net>
2022-08-30 15:34:34 +1000

    add problem enrichment

---
+++ problem-enrichment/assets/scripts/foreground.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#####################################
+# Please wait for install to finish #
+#####################################
+
+while [ ! -f /tmp/finished ]; do sleep 1; done
+
+#############################
+#   Installation complete   #
+#    Please continue...     #
+#############################
