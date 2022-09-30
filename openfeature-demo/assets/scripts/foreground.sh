74e92dbe7713b794f9557cd109dcdc571af7c998
agardnerit <adam@agardner.net>
2022-09-30 12:11:53 +1000

    apt not apt-get
    Signed-off-by: agardnerit <adam@agardner.net>

--- openfeature-demo/assets/scripts/foreground.sh
+++ openfeature-demo/assets/scripts/foreground.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-DEBUG_VERSION=5
+DEBUG_VERSION=6
 
 ###############################################
 # Step [1/3]: Install docker compose plugin
 ###############################################
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt update
 sudo apt install -y docker-compose-plugin
 
 ###############################################
