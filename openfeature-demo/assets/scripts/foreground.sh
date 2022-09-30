5f6114cd57c515ac9c57bacb8a0bcfacdf4b7fd6
agardnerit <adam@agardner.net>
2022-09-30 12:14:36 +1000

    add nulls
    Signed-off-by: agardnerit <adam@agardner.net>

--- openfeature-demo/assets/scripts/foreground.sh
+++ openfeature-demo/assets/scripts/foreground.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-DEBUG_VERSION=6
+DEBUG_VERSION=7
 
 ###############################################
 # Step [1/3]: Install docker compose plugin
 ###############################################
-sudo apt update
-sudo apt install -y ca-certificates curl gnupg lsb-release
+sudo apt update  < "/dev/null"
+sudo apt install -y ca-certificates curl gnupg lsb-release  < "/dev/null"
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-compose-plugin
+sudo apt update  < "/dev/null"
+sudo apt install -y docker-compose-plugin  < "/dev/null"
 
 ###############################################
 # Step [2/3]: Clone Repo and check out PR
