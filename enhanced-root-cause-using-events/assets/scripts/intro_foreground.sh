96f815ff675556776c6cc7349c85cd954d31659e
agardnerit <adam@agardner.net>
2022-10-05 10:06:51 +1000

    update foreground
    Signed-off-by: agardnerit <adam@agardner.net>

--- enhanced-root-cause-using-events/assets/scripts/intro_foreground.sh
+++ enhanced-root-cause-using-events/assets/scripts/intro_foreground.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
 
+DEBUG=1
+
+###########################
+# Installing components.  #
+# Please wait...          #
+###########################
+
 while [ ! -f /tmp/finished ]; do sleep 1; done
-echo "Installation Complete 🎉. Please proceed now."
+
+# "Installation Complete 🎉. Please proceed now."
