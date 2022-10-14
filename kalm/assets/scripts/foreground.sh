97e467de936c8bb99e799f2090ddb955e5c1c596
agardnerit <adam@agardner.net>
2022-10-14 16:15:45 +1000

    initial commit

---
+++ kalm/assets/scripts/foreground.sh
@@ -0,0 +1,10 @@
+wget https://nodejs.org/dist/v16.18.0/node-v16.18.0-linux-x64.tar.xz
+tar -xf node-v16.18.0-linux-x64.tar.xz
+mv node-v16.18.0-linux-x64/bin/node /usr/bin/node
+mv node-v16.18.0-linux-x64/bin/npm /usr/bin/npx
+
+curl -sSL https://install.python-poetry.org | python3 -
+
+git clone https://github.com/dynatrace-oss/Kalm-Benchmark
+
+poetry install
---
+++ kalm/intro.md
@@ -0,0 +1,3 @@
+# Intro page...
+
+TODO
