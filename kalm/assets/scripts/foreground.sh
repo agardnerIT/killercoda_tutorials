f696a1262dd3bbcb36b4ea34dce048cfe00b703c
agardnerit <adam@agardner.net>
2022-10-14 17:02:33 +1000

    add poetry path to bashrc

--- kalm/assets/scripts/foreground.sh
+++ kalm/assets/scripts/foreground.sh
@@ -1,3 +1,5 @@
+# DEBUG V1
+
 wget https://nodejs.org/dist/v16.18.0/node-v16.18.0-linux-x64.tar.xz
 tar -xf node-v16.18.0-linux-x64.tar.xz
 mv node-v16.18.0-linux-x64/bin/node /usr/bin/node
@@ -8,3 +10,6 @@ curl -sSL https://install.python-poetry.org | python3 -
 git clone https://github.com/dynatrace-oss/Kalm-Benchmark
 
 poetry install
+
+echo PATH=/root/.local/bin:$PATH > ~/.bashrc
+. ~/.bashrc
