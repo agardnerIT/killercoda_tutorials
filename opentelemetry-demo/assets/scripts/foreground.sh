c27d6ffa53e24f64702ecceab6dd3bb0846490c0
agardnerit <adam@agardner.net>
2022-10-22 09:29:15 +1000

    expose frontend and jaeger on LBs
    Signed-off-by: agardnerit <adam@agardner.net>

--- opentelemetry-demo/assets/scripts/foreground.sh
+++ opentelemetry-demo/assets/scripts/foreground.sh
@@ -1,6 +1,14 @@
+DEBUG_VERSION=2
+
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo update
-helm install otel-demo open-telemetry/opentelemetry-demo --version 0.9.6
+
+# Installing OpenTelemetry demo. This takes about 10 minutes
+helm install otel-demo open-telemetry/opentelemetry-demo \
+--version 0.9.6 \
+--wait \
+--set components.frontend.serviceType=LoadBalancer \
+--set observability.jaeger.serviceType=LoadBalancer
 
 # ---------------------------------------------#
 #       🎉 Installation Complete 🎉             #
