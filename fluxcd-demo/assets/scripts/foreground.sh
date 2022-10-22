3ae870e3d6de0f7e5edbb75d48a092ee1f565592
agardnerit <adam@agardner.net>
2022-10-22 11:10:43 +1000

    update

--- fluxcd-demo/assets/scripts/foreground.sh
+++ fluxcd-demo/assets/scripts/foreground.sh
@@ -1,2 +1,7 @@
-# TUTORIAL VERSION: 0.1.3
+# TUTORIAL VERSION: 0.1.4
 curl -s https://fluxcd.io/install.sh | sudo bash
+
+# ---------------------------------------------#
+#       🎉 Installation Complete 🎉             #
+#           Please proceed now...              #
+# ---------------------------------------------#
