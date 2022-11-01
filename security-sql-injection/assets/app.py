511b284f8e798f75e49b6537d85902204d9e16e9
agardnerit <adam@agardner.net>
2022-11-01 10:58:10 +1000

    sql injection initial commit
    Signed-off-by: agardnerit <adam@agardner.net>

---
+++ security-sql-injection/assets/app.py
@@ -0,0 +1,46 @@
+import sqlite3
+import os
+
+# 1. Enter "peters" (without double quotes). Result = no account found (case sensitive)
+# 2. Enter "Peters" (without double quotes). Result = Account found
+# 3. SQL Injection to retrieve data: Enter "fake' OR true --" (without double quotes). Result = SQL injection. All accounts shown.
+
+connection = sqlite3.connect("sql_injection_database.db")
+cursor = connection.cursor()
+
+def init_db():
+    with open('/app/schema.sql', mode='r') as f:
+        cursor.executescript(f.read())
+    connection.commit()
+
+init_db()
+
+exit = False
+while not exit:
+    input = input("Enter your surname: ")
+    if input == "exit": exit = True
+
+    select_statement = f"SELECT rowid, firstname, lastname, address FROM Accounts WHERE LastName='{input}'"
+    print(f"Here is the SELECT statement that will be executed")
+    print("-"*60)
+    print(select_statement)
+    print("-"*60)
+    print() # Empty line
+    try:
+        rows = cursor.execute(select_statement).fetchall()
+
+        if len(rows) < 1:
+            print("No Account found with that surname...");
+        else:
+            print("PRINTING OUTPUT")
+            print("="*60)
+            
+            for row in rows:
+                print(row)
+            
+            print("="*60)
+    except Exception as e:
+        print("Exception caught retrieving data from DB")
+        print(e)
+
+connection.close()
---
+++ security-sql-injection/intro.md
@@ -0,0 +1,9 @@
+# Security Basics: SQL Injection
+
+This tutorial will:
+
+- Explain what a SQL injection attack is
+- Show and allow you to run your own SQL injection attacks
+- Discuss ways to protect against SQL injection attacks
+
+Let's get started...
