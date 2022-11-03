1baa9e99479605ee8913a89a2121e75d25d8a6ac
agardnerit <adam@agardner.net>
2022-11-03 14:06:54 +1000

    bump version
    Signed-off-by: agardnerit <adam@agardner.net>

--- opentelemetry-dynatrace-1-otel-basics/assets/intro_background.sh
+++ opentelemetry-dynatrace-1-otel-basics/assets/intro_background.sh
@@ -1,3 +1,5 @@
+DEBUG_VERSION=2
+
 # Prepull images to improve startup speed
 docker pull ghcr.io/open-telemetry/opentelemetry-collector-contrib/tracegen:v0.63.0
 docker pull ghcr.io/open-telemetry/opentelemetry-collector-releases/opentelemetry-collector-contrib:0.63.0
