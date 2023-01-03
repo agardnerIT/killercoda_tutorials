f7f553987676d3bcfcfd6c521277a17dbcf8bab4
agardnerit <adam@agardner.net>
2023-01-03 13:51:34 +1000

    initial pyrsia commit
    Signed-off-by: agardnerit <adam@agardner.net>

---
+++ pyrsia/assets/scripts/intro_foreground.sh
@@ -0,0 +1,12 @@
+####################
+# Installing Pyrsia
+#
+# Please wait...
+####################
+
+while [ ! -f /tmp/finished ]; do sleep 1; done
+
+#################################
+# 🎉 Installation Complete 🎉
+# Please proceed...
+#################################
