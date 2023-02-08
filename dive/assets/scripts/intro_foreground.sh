6639d343d21594633f6b5ace070fc6a47e4b4e6d
agardnerit <adam@agardner.net>
2023-02-08 12:33:49 +1000

    add install complete msg

--- dive/assets/scripts/intro_foreground.sh
+++ dive/assets/scripts/intro_foreground.sh
@@ -1,3 +1,8 @@
 DIVE_VERSION=0.10.0
 wget https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_linux_amd64.deb
 sudo apt install ./dive_0.9.2_linux_amd64.deb
+
+#################################
+# 🎉 Installation Complete 🎉
+# Please proceed...
+#################################
