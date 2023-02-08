7f8246f8c4f9fd1e2d632eef3b7a215dd4c856c3
agardnerit <adam@agardner.net>
2023-02-08 12:57:28 +1000

    remove sudo

--- dive/assets/scripts/intro_foreground.sh
+++ dive/assets/scripts/intro_foreground.sh
@@ -1,6 +1,6 @@
 DIVE_VERSION=0.10.0
-wget https://github.com/wagoodman/dive/releases/download/v$DIVE_VERSION/dive_$DIVE_VERSION_linux_amd64.deb
-sudo apt install ./dive_$DIVE_VERSION_linux_amd64.deb
+wget https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb
+apt install ./dive_${DIVE_VERSION}_linux_amd64.deb
 
 #################################
 # 🎉 Installation Complete 🎉
