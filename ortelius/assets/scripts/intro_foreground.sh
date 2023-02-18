e87fb44d56b4f8e43b16ad617d050811198a9cc0
agardnerit <adam@agardner.net>
2023-02-18 18:21:50 +1000

    update

--- ortelius/assets/scripts/intro_foreground.sh
+++ ortelius/assets/scripts/intro_foreground.sh
@@ -1,6 +1,7 @@
 ORTELIUS_VERSION=10.0.97
 
-helm install -n ortelius --create-namespace ortelius https://github.com/ortelius/ortelius-charts/releases/download/ortelius-$ORTELIUS_VERSION/ortelius-$ORTELIUS_VERSION.tgz
+helm upgrade --install -n ortelius --create-namespace ortelius https://github.com/ortelius/ortelius-charts/releases/download/ortelius-$ORTELIUS_VERSION/ortelius-$ORTELIUS_VERSION.tgz \
+--set postgresql.primary.persistence.enabled=false
 
 #################################
 # 🎉 Installation Complete 🎉
