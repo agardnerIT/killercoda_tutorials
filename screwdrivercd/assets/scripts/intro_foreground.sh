9981402de639460695e38657a7e03b27886a074f
agardnerit <adam@agardner.net>
2023-02-26 15:45:29 +1000

    re-add screwdriver
    Signed-off-by: agardnerit <adam@agardner.net>

---
+++ screwdrivercd/assets/scripts/intro_foreground.sh
@@ -0,0 +1,5 @@
+docker pull docker.io/screwdrivercd/screwdriver:stable
+docker pull docker.io/screwdrivercd/ui:stable
+docker pull docker.io/screwdrivercd/store:stable
+
+python <(curl -L https://git.io/sd-in-a-box)
