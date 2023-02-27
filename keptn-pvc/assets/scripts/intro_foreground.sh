f60c256dd3f86193564c7617362bfc0e03abad00
agardnerit <adam@agardner.net>
2023-02-28 08:33:18 +1000

    install custom helm version

--- keptn-pvc/assets/scripts/intro_foreground.sh
+++ keptn-pvc/assets/scripts/intro_foreground.sh
@@ -10,6 +10,9 @@ KEPTN_PROMETHEUS_SERVICE_VERSION=0.9.1
 PROMETHEUS_VERSION=19.0.1
 POD_WAIT_TIMEOUT_MINS=10
 
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && chmod 700 get_helm.sh
+./get_helm.sh
+
 helm install keptn https://github.com/keptn/keptn/releases/download/$KEPTN_VERSION/keptn-$KEPTN_VERSION.tgz \
 -n keptn --create-namespace \
 --set mongo.resources.requests.cpu=0 \
