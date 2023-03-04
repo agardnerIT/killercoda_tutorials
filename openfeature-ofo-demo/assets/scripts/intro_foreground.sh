6fb8ff2dbed5dd0d6b236cd714663d1f46fcc0db
agardnerit <adam@agardner.net>
2023-03-04 13:49:01 +1000

    update
    Signed-off-by: agardnerit <adam@agardner.net>

--- openfeature-ofo-demo/assets/scripts/intro_foreground.sh
+++ openfeature-ofo-demo/assets/scripts/intro_foreground.sh
@@ -1,7 +1,7 @@
 
-DEBUG_VERSION=6
+DEBUG_VERSION=7
 DEMO_APP_PORT=30000
-
+CERT_MANAGER_VERSION=v1.10.1
 #########################################################
 # 1/3: Installing Cert Manager                          #
 #########################################################
@@ -11,7 +11,7 @@ helm install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
-  --version v1.10.1 \
+  --version ${CERT_MANAGER_VERSION} \
   --set installCRDs=true \
   --wait
 
