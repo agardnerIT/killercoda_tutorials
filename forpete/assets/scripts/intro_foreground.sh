156c9625f3e09bc3e5cece0c63a93264b7626ea1
agardnerit <adam@agardner.net>
2023-03-06 15:41:20 +1000

    fix titles and formatting

--- forpete/assets/scripts/intro_foreground.sh
+++ forpete/assets/scripts/intro_foreground.sh
@@ -1,15 +1,33 @@
-# DEBUG VERSION: 3
+# DEBUG VERSION: 4
 
+# -----------------------------------
+# Step 1/4: Installing Node
+# -----------------------------------
 curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
 apt install -y nodejs < /dev/null
+
+# -----------------------------------
+# Step 2/4: Installing jq
+# -----------------------------------
 apt install -y jq < /dev/null
 
+# -----------------------------------
+# Step 3/4: Installing NPM packages
+# -----------------------------------
 npm install express --save
 npm install express-promise-router --save
 npm install cowsay --save
 
+# -----------------------------------
+# Step 4/4: Initialising NPM package
+# -----------------------------------
 cd app
 npm init -y
 mv package.json package.BAK.json
 cat package.BAK.json | jq '. += { "type": "module" }' > package.json
+rm package.BAK.json
 
+# ---------------------------------------------#
+#       🎉 Installation Complete 🎉           #
+#           Please proceed now...              #
+# ---------------------------------------------#
