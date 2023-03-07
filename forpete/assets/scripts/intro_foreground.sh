da96412d1e4eae44a11c07a060edc59a2c306250
agardnerit <adam@agardner.net>
2023-03-08 09:20:17 +1000

    update

--- forpete/assets/scripts/intro_foreground.sh
+++ forpete/assets/scripts/intro_foreground.sh
@@ -6,7 +6,7 @@ apt update
 # -----------------------------------
 # Step 2/6: Installing bat
 # -----------------------------------
-apt install -y bat
+apt install -y bat < /dev/null
 # Symlink: Make 'batcat' available as 'bat' command
 ln -s /usr/bin/batcat /usr/sbin/bat
 # Alias 'cat' to 'bat' so running: 'cat ~/myfile.txt' uses 'bat' instead
