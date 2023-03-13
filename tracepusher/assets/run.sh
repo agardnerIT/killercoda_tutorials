1240e1e2ad0b815e3d1cb4699a226594b48fe3a7
agardnerit <adam@agardner.net>
2023-03-14 08:22:44 +1000

    subspan work
    Signed-off-by: agardnerit <adam@agardner.net>

--- tracepusher/assets/run.sh
+++ tracepusher/assets/run.sh
@@ -1,17 +1,47 @@
-#!/bin/bash
-echo "starting up..."
-sleep 2
-echo "done sleeping"
-END_TIME=$SECONDS
-echo "Duration: ${END_TIME}"
+#!/bin/sh
+time_now=$SECONDS
+echo "main() time now: ${time_now}"
 
-# Generate OTEL Trace
-echo "generating trace data for: killercoda / run.sh / ${END_TIME}s"
+# Generate parent trace id
+trace_id=$(openssl rand -hex 16)
+
+do_work() {
+    echo "doing work..."
+    echo "parent trace id: $1"
+    echo "loop counter: $2"
+    sleep 1
+    time_now=$SECONDS
+    echo "do_work() time now: ${time_now}"
+    # Push child span
+    echo "pushing child span with parent trace id set to ${1}"
+    docker run \
+    --add-host=host.docker.internal:host-gateway \
+    tp:ss \
+    --endpoint=http://host.docker.internal:4318 \
+    --service-name=killercoda \
+    --span-name=loop${2} \
+    --duration=${END_TIME} \
+    --time-shift=True \
+    --parent-trace-id=${1}
+}
+
+counter=1
+while [ $counter -le 2 ]
+do
+  do_work $trace_id $counter
+  counter=$((counter+1))
+done
+
+time_now=$SECONDS
+echo "main() end time_now: ${time_now}"
+
+echo "pushing main trace with trace_id: ${trace_id}"
 docker run \
 --add-host=host.docker.internal:host-gateway \
-gardnera/tracepusher:v0.4.0 \
+tp:ss \
 --endpoint=http://host.docker.internal:4318 \
 --service-name=killercoda \
 --span-name=run.sh \
 --duration=${END_TIME} \
---time-shift=True
+--time-shift=True \
+--trace-id=${trace_id}
--- tracepusher/assets/scripts/intro_foreground.sh
+++ tracepusher/assets/scripts/intro_foreground.sh
@@ -1,11 +1,16 @@
 OTELCOL_VERSION=0.73.0
 TRACEPUSHER_VERSION=v0.4.0
 
-docker pull gardnera/tracepusher:${TRACEPUSHER_VERSION}
+#docker pull gardnera/tracepusher:${TRACEPUSHER_VERSION}
 wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v${OTELCOL_VERSION}/otelcol-contrib_${OTELCOL_VERSION}_linux_amd64.tar.gz
 tar -xf otelcol-contrib_${OTELCOL_VERSION}_linux_amd64.tar.gz
 mv otelcol-contrib otelcol
 rm otelcol-contrib_${OTELCOL_VERSION}_linux_amd64.tar.gz
+
+git clone https://github.com/agardnerit/tracepusher
+cd tracepusher
+git checkout feat/subspans
+docker build -t tp:ss .
 
 # ---------------------------------------------#
 #       🎉 Installation Complete 🎉             #
