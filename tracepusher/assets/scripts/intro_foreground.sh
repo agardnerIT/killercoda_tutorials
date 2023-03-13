98fb985e12a97c464c36cf09bcec338b35b18e99
agardnerit <adam@agardner.net>
2023-03-13 21:20:23 +1000

    pre-pull docker image
    Signed-off-by: agardnerit <adam@agardner.net>

--- tracepusher/assets/scripts/intro_foreground.sh
+++ tracepusher/assets/scripts/intro_foreground.sh
@@ -1,5 +1,8 @@
 OTELCOL_VERSION=0.73.0
+TRACEPUSHER_VERSION=v0.4.0
 
+docker pull gardnera/tracepusher:${TRACEPUSHER_VERSION}
 wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v${OTELCOL_VERSION}/otelcol-contrib_${OTELCOL_VERSION}_linux_amd64.tar.gz
 tar -xf otelcol-contrib_${OTELCOL_VERSION}_linux_amd64.tar.gz
 mv otelcol-contrib otelcol
+rm otelcol-contrib_${OTELCOL_VERSION}_linux_amd64.tar.gz
