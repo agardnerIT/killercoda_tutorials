8246e33ac488f34d6c0fbf935818165525c0ff5d
agardnerit <adam@agardner.net>
2023-03-14 21:16:49 +1000

    update
    Signed-off-by: agardnerit <adam@agardner.net>

--- tracepusher/assets/run.sh
+++ tracepusher/assets/run.sh
@@ -4,44 +4,48 @@ echo "main() time now: ${time_now}"
 
 # Generate parent trace id
 trace_id=$(openssl rand -hex 16)
+span_id=$(openssl rand -hex 8)
 
 do_work() {
     echo "doing work..."
     echo "parent trace id: $1"
-    echo "loop counter: $2"
+    echo "parent span id: $2"
+    echo "loop counter: $3"
     sleep 1
     time_now=$SECONDS
     echo "do_work() time now: ${time_now}"
     # Push child span
-    echo "pushing child span with parent trace id set to ${1}"
+    echo "pushing child span with parent trace id set to ${1} and parent span id set to ${2}"
     docker run \
     --add-host=host.docker.internal:host-gateway \
     tp:ss \
     --endpoint=http://host.docker.internal:4318 \
     --service-name=killercoda \
     --span-name=loop${2} \
-    --duration=${END_TIME} \
+    --duration=${time_now} \
     --time-shift=True \
-    --parent-trace-id=${1}
+    --parent-span-id=${1} \
+    --trace-id=${3}
 }
 
 counter=1
 while [ $counter -le 2 ]
 do
-  do_work $trace_id $counter
+  do_work $trace_id $span_id $counter
   counter=$((counter+1))
 done
 
 time_now=$SECONDS
 echo "main() end time_now: ${time_now}"
 
-echo "pushing main trace with trace_id: ${trace_id}"
+echo "pushing main trace with trace_id: ${trace_id} and span id: ${span_id}"
 docker run \
 --add-host=host.docker.internal:host-gateway \
 tp:ss \
 --endpoint=http://host.docker.internal:4318 \
 --service-name=killercoda \
 --span-name=run.sh \
---duration=${END_TIME} \
+--duration=${time_now} \
 --time-shift=True \
---trace-id=${trace_id}
+--trace-id=${trace_id} \
+--span-id=${span_id}
