b979b863f60c4ad719c5567278968414e3ba5cc7
agardnerit <adam@agardner.net>
2023-03-15 14:58:46 +1000

    update

--- openfeature-custom-backend-flagd/assets/scripts/intro_foreground.sh
+++ openfeature-custom-backend-flagd/assets/scripts/intro_foreground.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DEBUG_VERSION=9
+DEBUG_VERSION=10
 GITEA_VERSION=1.19
 TEA_CLI_VERSION=0.9.2
 FLAGD_VERSION=0.4.4
 
 # Download and install flagd
-wget -O flagd.tar.gz https://github.com/open-feature/flagd/releases/download/v${FLAGD_VERSION}/flagd_${FLAGD_VERSION}_Linux_x86_64.tar.gz
+wget -O flagd.tar.gz https://github.com/open-feature/flagd/releases/download/flagd%2Fv${FLAGD_VERSION}/flagd_${FLAGD_VERSION}_Linux_x86_64.tar.gz
 tar -xf flagd.tar.gz
 chmod +x flagd
 mv flagd /usr/local/bin
