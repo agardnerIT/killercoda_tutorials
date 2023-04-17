7d3e49d3c37adff108e92990942bb85c7253d24f
Adam Gardner <adam@agardner.net>
2023-04-18 01:42:38 +1000

    update

--- testing-ground/assets/scripts/portforward.sh
+++ testing-ground/assets/scripts/portforward.sh
@@ -1,4 +1,4 @@
 echo "Port Forwarding. Access using Port 8080."
 echo ""
-nohup kubectl -n keptn port-forward --address 0.0.0.0 service/api-gateway-nginx 8080:80 &
+nohup kubectl -n keptn port-forward --address 0.0.0.0 service/api-gateway-nginx 8080:80 >/dev/null 2>&1
 echo "Please continue..."
