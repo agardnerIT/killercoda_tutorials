925b52f4cf886ef589d3114c7f08e4a418c9268a
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2023-05-28 15:10:03 +1000

    add install complete message
    Signed-off-by: Adam Gardner <26523841+agardnerIT@users.noreply.github.com>

--- tracepusherOSS/assets/intro_foreground.sh
+++ tracepusherOSS/assets/intro_foreground.sh
@@ -2,3 +2,8 @@ JAEGER_VERSION=1.45
 TRACEPUSHER_VERSION=v0.5.0
 docker pull jaegertracing/all-in-one:${JAEGER_VERSION}
 docker pull gardnera/tracepusher:${TRACEPUSHER_VERSION}
+
+# ---------------------------------------------#
+#       🎉 Installation Complete 🎉             #
+#           Please proceed now...              #
+# ---------------------------------------------#
