446c297ec642fef95365f4ad4328b1812b4f8807
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2023-05-29 16:31:24 +1000

    update script.sh
    Signed-off-by: Adam Gardner <26523841+agardnerIT@users.noreply.github.com>

--- tracepusherOSS/assets/script.sh
+++ tracepusherOSS/assets/script.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+echo "Starting script.sh"
+echo "This should take about 4 seconds to complete..."
+
 trace_id=$(hexdump -vn16 -e'4/4 "%08X" 1 "\n"' /dev/urandom)
 span_id=$(hexdump -vn8 -e'4/4 "%08X" 1 "\n"' /dev/urandom)
 
-echo "trace_id: ${trace_id}"
-echo "span_id: ${span_id}"
-
 main_time_start=0
-echo "main time_start: ${main_time_start}"
 
 counter=1
 limit=3
@@ -17,14 +16,12 @@ do
   # This is unique to this span
   sub_span_id=$(hexdump -vn8 -e'4/4 "%08X" 1 "\n"' /dev/urandom)
   time_start=$SECONDS
-  echo "loop: ${counter}"
   sleep 1
   time_end=$SECONDS
   duration=$(( $time_end - $time_start ))
-  echo "loop time_start: ${time_start}. time_end: ${time_end}. duration: ${duration}"
 
   docker run --network demo gardnera/tracepusher:v0.5.0 \
-    --endpoint=http://localhost:4318 \
+    --endpoint=http://jaeger:4318 \
     --service-name=serviceA \
     --span-name="subspan${counter}" \
     --duration=${duration} \
@@ -32,7 +29,6 @@ do
     --parent-span-id=${span_id} \
     --span-id=${sub_span_id} \
     --time-shift=True &
-  echo "pushing subspan: ${sub_span_id} with span name: subspan${counter}. trace id: ${trace_id} and parent span id: ${span_id} and time shifted"
 
   counter=$(( $counter + 1 ))
   
@@ -40,15 +36,15 @@ done
 
 time_end=$SECONDS
 
-echo "main time_start: ${main_time_start}. time_end: ${time_end}"
-
-echo "pushing main_trace with duration: ${time_end} and trace_id: ${trace_id} and span_id=${span_id} and time shifted"
-
 docker run --network demo gardnera/tracepusher:v0.5.0 \
-  --endpoint http://localhost:4318 \
+  --endpoint http://jaeger:4318 \
   --service-name service1 \
   --span-name script.sh \
   --duration 2 \
   --trace-id=${trace_id} \
   --span-id=${span_id} \
   --time-shift=True &
+
+echo "================================="
+echo "script.sh completed successfully."
+echo "================================="
--- tracepusherOSS/step2.md
+++ tracepusherOSS/step2.md
@@ -7,7 +7,7 @@ Now use tracepusher to trace a shell script.
 ~/script.sh
 ```{{exec}}
 
-Open Jaeger again to see the trace (you may need to refresh the page).
+[Open Jaeger again](({{TRAFFIC_HOST1_16686}}/search?service=service1)) (or click "Find Traces" to refresh the trace list) to see the trace (you may need to refresh the page).
 
 View what the shell script looks like, either open the Editor window or click this:
 
