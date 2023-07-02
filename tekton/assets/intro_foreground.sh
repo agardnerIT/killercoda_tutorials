e3725f51cb858dd197995a56992ed642870ef82f
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2023-07-03 08:38:27 +1000

    update
    Signed-off-by: Adam Gardner <26523841+agardnerIT@users.noreply.github.com>

---
+++ tekton/assets/intro_foreground.sh
@@ -0,0 +1,12 @@
+DEBUG_VERSION=2
+POD_WAIT_TIMEOUT_MINS=5
+
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+
+kubectl -n tekton-pipelines wait --for condition=Available=True --timeout=${POD_WAIT_TIMEOUT_MINS}m deployment/tekton-pipelines-controller
+kubectl -n tekton-pipelines wait --for condition=Available=True --timeout=${POD_WAIT_TIMEOUT_MINS}m deployment/tekton-pipelines-webhook
+
+#############################
+#   Installation complete   #
+#    Please continue...     #
+#############################
