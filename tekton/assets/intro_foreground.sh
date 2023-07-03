64d6971896e267fc481d3a0a9824f2dd997774e3
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2023-07-03 10:30:13 +1000

    update
    Signed-off-by: Adam Gardner <26523841+agardnerIT@users.noreply.github.com>

--- tekton/assets/intro_foreground.sh
+++ tekton/assets/intro_foreground.sh
@@ -1,5 +1,10 @@
-DEBUG_VERSION=4
+DEBUG_VERSION=6
 POD_WAIT_TIMEOUT_MINS=5
+TEKTON_CLI_VERSION=0.31.1
+
+wget https://github.com/tektoncd/cli/releases/download/v${TEKTON_CLI_VERSION}/tkn_${TEKTON_CLI_VERSION}_Linux_x86_64.tar.gz
+tar -xf tkn_${TEKTON_CLI_VERSION}_Linux_x86_64.tar.gz
+sudo mv tkn /usr/bin
 
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 
