f3e3a5c5f764969e735342c6bbd8bf6c0a6d7a33
Adam Gardner <adam@agardner.net>
2024-01-03 21:47:29 +1000

    dynamic url

--- testing-ground/assets/scripts/intro_foreground.sh
+++ testing-ground/assets/scripts/intro_foreground.sh
@@ -1,4 +1,4 @@
-DEBUG_VERSION=1
+DEBUG_VERSION=2
 DEMO_APP_PORT=30000
 CERT_MANAGER_VERSION=v1.13.2
 OPENFEATURE_OPERATOR_VERSION=v0.5.3
@@ -22,7 +22,14 @@ kubectl apply -f ~/open-feature-operator-namespace.yaml
 kubectl apply -f https://github.com/open-feature/open-feature-operator/releases/download/v0.5.3/release.yaml && \
 kubectl wait --timeout=60s --for condition=Available=True deploy --all -n 'open-feature-operator-system'
 
+# Get external URL
+# Format: abc1234-PORT.papa.r.killercoda.com
+FLAGD_HOST_WEB=$(cat /etc/killercoda/host)
+# replace string literal "PORT" with the port (30002)
+FLAGD_HOST_WEB_REPLACED=$(echo "${FLAGD_HOST_WEB/PORT/"30002"}")
+# Overwrite placeholder with dynamic value in ~/end-to-end.yaml
+sed -i "s/FLAGD_HOST_WEB_PLACEHOLDER/$FLAGD_HOST_WEB_REPLACED/g" ~/end-to-end.yaml
+
 # Apply workload and config
 kubectl apply -f ~/end-to-end.yaml
-sleep 10
-kubectl wait pods -n open-feature-demo -l app=open-feature-demo --for condition=Ready --timeout=30s
+kubectl wait --timeout=60s deployment --for condition=Available=True -l 'app=open-feature-demo' -n 'default'
