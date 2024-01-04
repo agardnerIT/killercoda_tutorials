7990509d68de4cb1bebd691f4fbf9115772d1973
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-01-04 12:20:15 +1000

    fix OFO killercoda

--- testing-ground/intro_foreground.sh
+++ testing-ground/intro_foreground.sh
@@ -1,13 +1,28 @@
-# Update system and install base tooling
-sudo apt-get update
-sudo apt-get install -y wget gnupg
+DEBUG_VERSION=1
+DEMO_APP_PORT=30000
+CERT_MANAGER_VERSION=v1.13.2
+OPENFEATURE_OPERATOR_VERSION=v0.5.3
+#########################################################
+# 1/3: Installing Cert Manager                          #
+#########################################################
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version ${CERT_MANAGER_VERSION} \
+  --set installCRDs=true \
+  --wait
 
-# Add the Pyrsia keys to verify packages
-wget -q -O - https://repo.pyrsia.io/repos/Release.key |  gpg --dearmor  > pyrsia.gpg
-sudo install -o root -g root -m 644 pyrsia.gpg /etc/apt/trusted.gpg.d/
-rm pyrsia.gpg
-echo "deb https://repo.pyrsia.io/repos/stable focal main" | sudo tee -a /etc/apt/sources.list > /dev/null
-sudo apt-get update
+# Create OFO namespace
+kubectl apply -f ~/open-feature-operator-namespace.yml
 
-# Install
-sudo apt-get install -y pyrsia
+# Install OFO
+helm repo add openfeature https://open-feature.github.io/open-feature-operator/
+helm install ofo openfeature/open-feature-operator --version ${OPENFEATURE_OPERATOR_VERSION} --wait
+
+# Apply workload and config
+kubectl apply -f ~/end-to-end.yaml
+sleep 10
+kubectl wait pods -n open-feature-demo -l app=open-feature-demo --for condition=Ready --timeout=30s
